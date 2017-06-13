031ee1c029a546432f56bf8b5166c6100b63b5b9
derekjgrove <derekjgrove@gmail.com>
2017-06-12 21:38:37 -0400

    Add files via upload

---
+++ js/maps.js
@@ -0,0 +1,25 @@
+      function initMap() {
+              var myLatLng = {lat: 40.371142, lng: -79.692584};
+
+              var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 12,
+                center: myLatLng
+              });
+
+              var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: 'DG\'s Greener Side\n\n230 Sunrise Dr.\nIrwin PA, 15642'
+              });
+
+              var circle = new google.maps.Circle({
+                  strokeColor: '#1472FF',
+                  strokeOpacity: 0.8,
+                  strokeWeight: 2,
+                  fillColor: '#0F6CAA',
+                  fillOpacity: 0.35,
+                  map: map,
+                  center: {lat: 40.371142, lng: -79.692584},
+                  radius: 8046.72
+                });
+            }
