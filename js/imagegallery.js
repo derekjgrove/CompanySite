e9986dca25ad5d01c30844bda27e36ab81d0ecf4
Derek Grove <derek.grove@pncilab.com>
2017-09-12 22:52:53 -0400

    image gallery update5

--- js/imagegallery.js
+++ js/imagegallery.js
@@ -5,7 +5,7 @@ $(window).on('load', function() {
   var newImageURL = "./img/pictures/" + imageCount + ".jpg";
   var images = "";
 
-  while(doesExist(newImageURL) == true) {
+  while(doesExist(newImageURL) === true) {
     images += '<li><img src="' + url + '" /></li>';
     imageCount++;
   }
