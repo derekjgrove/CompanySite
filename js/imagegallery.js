468b9761dc4797675f2bbc7a221b1923f2bb85eb
Derek Grove <derek.grove@pncilab.com>
2017-09-13 00:46:47 -0400

    image gallery update15

--- js/imagegallery.js
+++ js/imagegallery.js
@@ -8,7 +8,7 @@ $.ajax({
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpe?g|png|gif)$/) ) {
-                $("#carousel ul").append( "<li><img src='"+ folder + val +"'/></li>" );
+                $("#carousel ul").append( "<li class='flex-active-slide' style='width: 80px; margin-right: 5px; float: left; display: block;'><img draggable='false' src='"+ folder + val +"'/></li>" );
                 console.log(folder+val);
             }
         });
